819806e89077b863dfc5b557cb7392cfc9276360
Anthony Smith <antsmithdev@gmail.com>
2025-04-07 14:54:34 -0700

    Initial Commit

---
+++ StudySites/StudySitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySitesApp.swift
+//  StudySites
+//
+//  Created by Anthony Smith on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct StudySitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudySitesTests/StudySitesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudySitesTests.swift
+//  StudySitesTests
+//
+//  Created by Anthony Smith on 4/7/25.
+//
+
+import Testing
+@testable import StudySites
+
+struct StudySitesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
